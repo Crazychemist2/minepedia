8ee4d42a8e3defb3564b60c54363e9d060ee3e25
PauMAVA <paumachetti@gmail.com>
2019-05-06 11:41:14 +0200

    19w19a

--- src/org/Bread297/minepedia/HTMLParser.java
+++ src/org/Bread297/minepedia/HTMLParser.java
@@ -13,16 +13,29 @@ public class HTMLParser {
 	
 	public static void getSearchResults() {
 		File input = new File("\\plugins\\Minepedia\\index.html");
-		try {
-			Document doc = Jsoup.parse(input, "UTF-8");
-			Elements results = doc.getElementsByTag("a");
-			for(Element result : results) {
-				String resultText = result.attr("title");
-				Bukkit.broadcastMessage(resultText);
+			try {
+				Document doc = Jsoup.parse(input, "UTF-8");
+				Element body = doc.body();
+				Elements globalWrapper = body.select("div#global-wrapper");
+				Elements pageWrapper = globalWrapper.select("div#page-wrapper");
+				Elements content = pageWrapper.select("div#content");
+				Elements bodyContent = content.select("div#bodyContent");
+				Elements contentText = bodyContent.select("div#mw-content-text");
+				Elements searchResults = contentText.select("div.searchresults");
+				Elements ulResults = searchResults.select("ul.mw-search-results");
+				Elements liList = ulResults.select("li");
+				Elements resultHeading = liList.select("div.mw-search-result-heading");
+				Elements aList = resultHeading.select("a");
+				int i = 0;
+				String[] results = null;
+				for(Element a : aList) {
+					
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
 			}
-		} catch (IOException e) {
-			return;
-		}
+			
 		
 		
 		
